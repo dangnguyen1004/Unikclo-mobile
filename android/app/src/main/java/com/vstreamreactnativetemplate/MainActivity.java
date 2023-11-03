55335dbfafa5bce06698de80d228107560ec3505
DangNguyen <nguyendangit04@gmail.com>
2023-11-03 13:54:51 +0700

    chore: Renamed to Unikclo

--- android/app/src/main/java/com/vstreamreactnativetemplate/MainActivity.java
+++ android/app/src/main/java/com/vstreamreactnativetemplate/MainActivity.java
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "VStreamReactNativeTemplate";
+    return "Unikclo";
   }
 
     //react-native-screens override
--- ios/UnikcloTests/UnikcloTests.m
+++ ios/UnikcloTests/UnikcloTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface VStreamReactNativeTemplateTests : XCTestCase
+@interface UnikcloTests : XCTestCase
 
 @end
 
-@implementation VStreamReactNativeTemplateTests
+@implementation UnikcloTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
