b0333ca980ca2dfd8bfcdbef82d5c3e30918f950
DangNguyen <nguyendangit04@gmail.com>
2023-11-09 14:04:13 +0700

    feat(app): Added keyboardavoidingview

--- App.tsx
+++ App.tsx
@@ -1,31 +1,41 @@
 import React from 'react';
-import {Platform, SafeAreaView, StatusBar, StyleSheet} from 'react-native';
-import {colors} from './src/constants/colors';
-import {Router} from './src/routes';
+import {
+  KeyboardAvoidingView,
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+} from 'react-native';
+import {PaperProvider} from 'react-native-paper';
 import 'react-native-reanimated';
 import {Provider} from 'react-redux';
-import {persister, store} from './src/redux/store';
 import {PersistGate} from 'redux-persist/integration/react';
-import {PaperProvider} from 'react-native-paper';
+import {colors} from './src/constants/colors';
+import {persister, store} from './src/redux/store';
+import {Router} from './src/routes';
 
 function App(): JSX.Element {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persister}>
         <PaperProvider>
-          <SafeAreaView style={styles.container}>
-            <StatusBar
-              barStyle={
-                Platform.OS === 'ios' ? 'dark-content' : 'light-content'
-              }
-              backgroundColor={
-                Platform.OS === 'ios'
-                  ? colors.palette.neutral100
-                  : colors.palette.neutral200
-              }
-            />
-            <Router />
-          </SafeAreaView>
+          <KeyboardAvoidingView
+            style={styles.container}
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+            <SafeAreaView style={styles.container}>
+              <StatusBar
+                barStyle={
+                  Platform.OS === 'ios' ? 'dark-content' : 'light-content'
+                }
+                backgroundColor={
+                  Platform.OS === 'ios'
+                    ? colors.palette.neutral100
+                    : colors.palette.neutral200
+                }
+              />
+              <Router />
+            </SafeAreaView>
+          </KeyboardAvoidingView>
         </PaperProvider>
       </PersistGate>
     </Provider>
